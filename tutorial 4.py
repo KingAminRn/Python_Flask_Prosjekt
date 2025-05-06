dbf9f29cf72de9207bea4b18db2908c76217db78
Amin <alizadehamin31@gmail.com>
2025-05-06 09:46:35 +0200

    get and post + new page

--- tutorial 4.py
+++ tutorial 4.py
@@ -8,6 +8,11 @@ def home():
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
